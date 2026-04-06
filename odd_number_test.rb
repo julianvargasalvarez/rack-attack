31be1a0579bcf7dea6a55ae1649736b4eb260283
Julian Vargas <julianvargasalvarez@gmail.com>
2026-04-06 23:19:32 +0200

    Adds the first implementation

--- odd_number_test.rb
+++ odd_number_test.rb
@@ -1,7 +1,11 @@
 require 'test/unit'
 
+def is_odd(number)
+  true
+end
+
 class TestOddNumber < Test::Unit::TestCase
   def test_is_true_for_one
-    assert_equal(false, true)
+    assert_equal(true, is_odd(1))
   end
 end
